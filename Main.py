754d4adbb52a884490b4a1beb66809210d65657e
FranciscoPagoaga <francisco.pagoaga14@gmail.com>
2022-05-10 11:19:52 -0600

    Creacion json y llenar estructura de datos

--- Main.py
+++ Main.py
@@ -1,12 +1,26 @@
 from Undirected_Graph import Vertex, Graph
+import json
+
+def getJson():
+    with open("SimplifiedMap.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
+    return jsonObject
+
+def createGraph(graph):
+    jsonObject = getJson()
+
+    for name in jsonObject['cities']:
+        v= Vertex(name.get('name'))
+        graph.addVertex(v)
+
+    for edge in jsonObject['connections']:
+        firstCity = edge.get('cityA')
+        secondCity = edge.get('cityB')
+        weight = edge.get('weight')
+        graph.addEdge(firstCity, secondCity, weight)
 
 if __name__ == '__main__':
-    print("hola")
-    g = Graph()
-    for a in range(5):
-        v = Vertex(str(a))
-        g.addVertex(v)
-
-    g.addEdge("1","2", 5)
-    g.addEdge("3","4", 10)
-    g.printGraph()
+    graph = Graph()
+    createGraph(graph)
+    graph.printGraph()
